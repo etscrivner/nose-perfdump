84ba94ab1587b050309e9aa834b99d5f9819da30
Eric Scrivner <eric@grapheffect.com>
2012-11-29 20:50:10 -0800

    Refactor Connection and Create Timing Models

---
+++ perfdump/models.py
@@ -0,0 +1,93 @@
+import sqlite3
+
+from perfdump.connection import SqliteConnection
+
+
+class BaseTimeModel(object):
+    """Base class for a time sample."""
+    
+    @classmethod
+    def create(cls, file, module, cls_name, func, elapsed):
+        """Inserts a new time sample into the database with the given information.
+        
+        :param file: The file the sample is from
+        :type file: str
+        :param module: The module the sample is from
+        :type module: str
+        :param cls_name: The name of the class the sample is from
+        :type cls_name: str
+        :param func: The name of the function the sample is from
+        :type func: str
+        :param elapsed: The elapsed time in seconds
+        :type elapsed: float
+        
+        """
+        q = "INSERT INTO {} VALUES('{}', '{}', '{}', '{}', {})"
+        SqliteConnection.get().execute(q.format(cls.meta['table'],
+                                                file,
+                                                module,
+                                                cls_name,
+                                                func,
+                                                elapsed))
+    
+    @classmethod
+    def get_cursor(cls):
+        """Return a message list cursor that returns sqlite3.Row objects"""
+        db = SqliteConnection.get()
+        db.row_factory = sqlite3.Row
+        return db.cursor()
+    
+    @classmethod
+    def get_slowest_tests(cls, num=10):
+        """Returns the slow num tests.
+        
+        :param num: The maximum number of results to be returned.
+        :type num: int
+        
+        """
+        cur = cls.get_cursor()
+        q = "SELECT * FROM {} ORDER BY elapsed DESC LIMIT {}"
+        cur.execute(q.format(cls.meta['table'], num))
+        return cur.fetchall()
+    
+    @classmethod
+    def get_slowest_files(cls, num=10):
+        """Returns the slowest num files.
+        
+        :param num: The maximum number of results to be returned
+        :type num: int
+        
+        """
+        cur = cls.get_cursor()
+        q = "SELECT file, SUM(elapsed) as sum_elapsed FROM {} ORDER BY sum_elapsed DESC LIMIT {}"
+        cur.execute(q.format(cls.meta['table'], num))
+        return cur.fetchall()
+    
+    @classmethod
+    def get_total_time(cls):
+        """Returns the total time taken across all results.
+        
+        :rtype: float
+        
+        """
+        cur = cls.get_cursor()
+        q = "SELECT SUM(elapsed) FROM {}"
+        cur.execute(q.format(cls.meta['table']))
+        return cur.fetchone()['SUM(elapsed)']
+    
+    
+class TestTime(BaseTimeModel):
+    """Represents a time metric for a single test."""
+    
+    meta = {
+        'table': 'times'
+    }
+    
+
+class SetupTime(BaseTimeModel):
+    """Represents a time metric for a single test setup."""
+    
+    meta = {
+        'table': 'setup_times'
+    }
+    
--- perfdump/plugin.py
+++ perfdump/plugin.py
@@ -7,25 +7,21 @@ import time
 import nose
 from nose.plugins import Plugin
 
-
-def get_connection():
-    try:
-        os.remove('perfdump.db')
-    except:
-        pass
-    sqlite_connection = sqlite3.connect('perfdump')
-    sqlite_connection.execute("CREATE TABLE IF NOT EXISTS times (file text, module text, class text, func text, elapsed float)")
-    return sqlite_connection
+from perfdump.connection import SqliteConnection
+from perfdump.models import TestTime
 
 
 class PerfDumpPlugin(Plugin):
-    """
-    Nose plugin that will collect and write to an SQLite database per-test
-    elapsed times and print out the slowest 10 tests in your codebase.
-    """
+    """Nose plugin that will collect and write to an SQLite database per-test
+    elapsed times and print out the slowest 10 tests in your codebase."""
+    
     name = 'perfdump'
     times = {}
 
+    def __init__(self):
+        super(PerfDumpPlugin, self).__init__()
+        self.database_name = 'perfdump'
+        
     def get_test_file(self, test):
         """Returns the partial path from the current working directory for the
         file containing the given test.
@@ -75,7 +71,7 @@ class PerfDumpPlugin(Plugin):
         super(PerfDumpPlugin, self).configure(options, conf)
         if not self.enabled:
             return
-        self.db = get_connection()
+        self.db = SqliteConnection.get(self.database_name)
 
     def beforeTest(self, test):
         """Records the base time before the test is run."""
@@ -85,12 +81,11 @@ class PerfDumpPlugin(Plugin):
         """Records the complete test performance information after it is run"""
         elapsed = time.clock() - self.times[test.id()]
         del self.times[test.id()]
-        q = "INSERT INTO times VALUES('{}', '{}', '{}', '{}', {})"
-        self.db.execute(q.format(self.get_test_file(test),
-                                 self.get_module_name(test),
-                                 self.get_class_name(test),
-                                 self.get_function_name(test),
-                                 elapsed))
+        TestTime.create(self.get_test_file(test),
+                        self.get_module_name(test),
+                        self.get_class_name(test),
+                        self.get_function_name(test),
+                        elapsed)
 
     def report(self, stream):
         """Displays the slowest tests"""
@@ -98,36 +93,31 @@ class PerfDumpPlugin(Plugin):
 
         stream.writeln()
 
-        self.db.row_factory = sqlite3.Row
-        cur = self.db.cursor()
-        cur.execute("SELECT * FROM times ORDER BY elapsed DESC LIMIT 10")
-        row = cur.fetchone()
-        while row:
-            stream.writeln('{:.04f}s {}'.format(row['elapsed'],
+        # Display the slowest individual tests
+        slowest_tests = TestTime.get_slowest_tests(10)
+        for row in slowest_tests:
+            stream.writeln('{:.05f}s {}'.format(row['elapsed'],
                                                 row['file'])) 
-            stream.writeln('{:8}{}.{}.{}'.format('',
+            stream.writeln('{:9}{}.{}.{}'.format('',
                                                  row['module'],
                                                  row['class'],
                                                  row['func']))
             stream.writeln()
-            row = cur.fetchone()
 
         stream.writeln('-'*10)
         stream.writeln()
-        
-        cur.execute('SELECT file, SUM(elapsed) FROM times GROUP BY file ORDER BY SUM(elapsed) DESC LIMIT 10')
-        row = cur.fetchone()
-        while row:
-            stream.writeln('{:.04f}s {}'.format(row['SUM(elapsed)'],
+
+        # Display the slowest test files
+        slowest_files = TestTime.get_slowest_files(10)
+        for row in slowest_files:
+            stream.writeln('{:.05f}s {}'.format(row['sum_elapsed'],
                                                 row['file']))
             stream.writeln()
-            row = cur.fetchone()
 
-        cur.execute('SELECT SUM(elapsed) FROM times')
-        row = cur.fetchone()
+        # Display the total time spent in tests
         stream.writeln('-'*10)
         stream.writeln()
-        stream.writeln('Total time: {}s'.format(row['SUM(elapsed)']))
+        stream.writeln('Total time: {:.05f}s'.format(TestTime.get_total_time()))
         
         stream.writeln()
     
